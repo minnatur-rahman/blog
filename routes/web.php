dcfb0c094466cc9bff2f58eab01641f230a8fc37
millatur <minnatur7373@gmail.com>
2023-12-06 06:41:16 +0600

    molin deit data

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,12 @@ Route::get('/', function(){
     return view('index');
 });
 
-Route::get('/post/{id?}', function(string $id=null){
+Route::get('/post/{id}', function (string $id){
     if($id){
         return "<h1>Hi I am New Id : " .$id.  " </h1>";
     }else{
         return "<h1>No Id Found</h1>";
     }
 
-
-
-
-});
+})->whereIn('id', ['Movie', 'song', 'painting']);
 
