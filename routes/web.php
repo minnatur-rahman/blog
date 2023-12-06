684926bf6b974e4b3750b4120e959eac6cd84d14
millatur <minnatur7373@gmail.com>
2023-12-06 06:06:03 +0600

    my today first commit e

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,15 @@ Route::get('/', function(){
     return view('index');
 });
 
-Route::get('/post/{id}', function(string $id){
-    return "<h1>Hi I am New Id : " .$id.  " </h1>";
+Route::get('/post/{id?}', function(string $id=null){
+    if($id){
+        return "<h1>Hi I am New Id : " .$id.  " </h1>";
+    }else{
+        return "<h1>No Id Found</h1>";
+    }
+
+
+
 
 });
 
