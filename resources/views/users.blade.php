804b21773a787511fbcdc116960ac76a991b1199
millatur <minnatur7373@gmail.com>
2023-12-12 21:59:38 +0600

    today my third commit e

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,6 +1,10 @@
 <h1>Users Pages</h1>
 
 
-<h2> {{ $user }} </h2>
+{{-- <h2> {{ $user }} </h2>
 
-<h2>City :  {{ !empty($city) ? $city : 'No City' }} </h2>
+<h2>City :  {{ !empty($city) ? $city : 'No City' }} </h2> --}}
+
+@foreach ($user as $id => $u)
+    <h3> {{ $u['name'] }} | {{ $u['phone'] }} | {{ $u['city'] }}</h3>
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/pon', function(){
 
 
     return view('users', [
-        'user' => $name,
+        'user' => $names,
         'city' => 'Dhaka'
     ]);
 
